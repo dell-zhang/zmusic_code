1f20b68d22781c5dda225a444daca90438e69b46
Dell Zhang <dell.z@ieee.org>
2012-08-04 01:24:48 +0100

    .

--- model.py
+++ model.py
@@ -32,6 +32,7 @@ def represent(example):
     user_id   = example['user']
     time_id   = example['time']
     vector.extend(indicator(artist_id, 50))
+    vector.extend(indicator(track_id, 184))
     vector.extend(indicator(time_id, 24))
     user = users.user_dict.get(user_id, defaultdict(lambda: -1))
     vector.append(binary(user['gender']))
@@ -39,14 +40,14 @@ def represent(example):
     vector.extend(indicator(user['working'], 13))
     vector.extend(indicator(user['region'], 4))
     vector.extend(indicator(user['music'], 5))
-    vector.extend([user['list_own']/24.0, user['list_back']/24.0])
+    vector.append(user['list_own'] /24.0)
+    vector.append(user['list_back']/24.0)
     vector.extend([user['q%d' % (j+1)]/100.0 for j in range(19)])
     word = words.word_dict.get((artist_id, user_id), defaultdict(lambda: -1))
     vector.extend(indicator(word['heard-of'], 4))
     vector.extend(indicator(word['own-artist-music'], 5))
     vector.append(word['like-artist']/100.0)
     vector.extend([binary(word['w%d' % (j+1)]) for j in range(81)])
-    vector.extend(indicator(track_id, 184))
     vector.append(user_id/50928.0)
     # The principled way is to use vector.extend(indicator(user_id, 50928)), 
     # but it would take too much memory.
